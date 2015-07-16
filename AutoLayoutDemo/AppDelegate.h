1dd5a0dd9fa44625434aad5db3ad8f4a2f31fc48
ebaotong <2455063164@qq.com>
2015-07-16 11:43:34 +0800

    几个view等宽等高

---
+++ AutoLayoutDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutDemo
+//
+//  Created by ebaotong on 15/7/16.
+//  Copyright (c) 2015年 com.csst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayoutDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutDemo
+//
+//  Created by ebaotong on 15/7/16.
+//  Copyright (c) 2015年 com.csst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
